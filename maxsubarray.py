0839ff1f975e772f831448497ec55f5e94ee5a36
Jaichand Mulakalapalli <mjaichandms@gmail.com>
2023-02-23 13:33:18 -0600

    Updated file with Kadane algorithm

--- maxsubarray.py
+++ maxsubarray.py
@@ -8,6 +8,14 @@ def max_subarray_brute_force(A):
                 maxsum = total
     return maxsum
 
+def max_subarray_kadane(A):
+    max_end_here = max_far_end = A[0]
+    for x in A[1:]:
+        max_end_here = max(x, max_end_here + x)
+        max_far_end = max(max_far_end, max_end_here)
+    return max_far_end
+
 A=[-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
-print(max_subarray_brute_force(A))
+print("Max sub array using Brute Froce: ",max_subarray_brute_force(A))
+print("Max sub array using Kadane Froce: ",max_subarray_kadane(A))
