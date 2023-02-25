413b6619af54b6341a1dff703679dc1414e2a2de
Jaichand Mulakalapalli <mjaichandms@gmail.com>
2023-02-24 21:51:58 -0600

    added one more hand test case

--- maxsubarray_test.py
+++ maxsubarray_test.py
@@ -4,13 +4,17 @@ import random
 from maxsubarray import max_subarray_brute_force
 
 class TestMaxSubarray(unittest.TestCase):
-    def generated_hand_test_1(self):
+    def test_generated_by_hand_1(self):
         A = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
         self.assertEqual(max_subarray_brute_force(A), 6)
 
-    def generated_hand_test_2(self):
+    def test_generated_by_hand_2(self):
         A = [-1, -2, -3, -4]
         self.assertEqual(max_subarray_brute_force(A), -1)
+        
+    def test_generated_by_hand_3(self):
+        A = [2,3,-2,-6, -4,-1,3,8]
+        self.assertEqual(max_subarray_brute_force(A), 11)
 
 if __name__ == '__main__':
     unittest.main()
