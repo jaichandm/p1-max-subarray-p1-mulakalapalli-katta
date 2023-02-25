b6a9b2ff3d6fa1d1393185562ae01b2759fed639
Jaichand Mulakalapalli <mjaichandms@gmail.com>
2023-02-24 18:14:19 -0600

    Removed print statements

--- maxsubarray.py
+++ maxsubarray.py
@@ -14,8 +14,3 @@ def max_subarray_kadane(A):
         max_end_here = max(x, max_end_here + x)
         max_far_end = max(max_far_end, max_end_here)
     return max_far_end
-
-A=[-2, 1, -3, 4, -1, 2, 1, -5, 4]
-
-print("Max sub array using Brute Froce: ",max_subarray_brute_force(A))
-print("Max sub array using Kadane Froce: ",max_subarray_kadane(A))
