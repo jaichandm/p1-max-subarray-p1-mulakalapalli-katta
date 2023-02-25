7789f644e2c0673d933f24617b69d0b45f4c0fda
Narendar reddy katta <98133821+Narendarkatta@users.noreply.github.com>
2023-02-24 18:49:08 -0600

    Update maxsubarray
    Iterate over sizes code part

--- maxsubarray_test_timing.py
+++ maxsubarray_test_timing.py
@@ -11,3 +11,22 @@ num_iter = 100
 
 # Results dictionary
 results = {'brute': {}, 'kadane': {}}
+
+# Iterate over sizes
+for size in sizes:
+    # Generate random array list A
+    A = [random.randint(-100, 100) for _ in range(size)]
+
+    # Time brute force algorithm
+    start_time = timeit.default_timer()
+    for _ in range(num_iter):
+        max_subarray_brute_force(A)
+    end_time = timeit.default_timer()
+    results['brute'][size] = end_time - start_time
+
+    # Time Kadane's algorithm
+    start_time = timeit.default_timer()
+    for _ in range(num_iter):
+        max_subarray_kadane(A)
+    end_time = timeit.default_timer()
+    results['kadane'][size] = end_time - start_time
