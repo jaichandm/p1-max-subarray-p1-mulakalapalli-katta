fd1e134ca214ffe9eb46de02233ff9466ffad254
Narendarkatta <k.narendarreddy111@gmail.com>
2023-03-03 13:31:17 -0600

    update maxsubarray_test_timing.py
    Kadane's algorithm

--- maxsubarray_test_timing.py
+++ maxsubarray_test_timing.py
@@ -1,7 +1,7 @@
 import timeit
 import random
 
-from maxsubarray import max_subarray_brute_force
+from maxsubarray import max_subarray_brute_force, max_subarray_kadane
 
 # Sizes of array list A
 sizes = [10, 50, 100, 250, 500]
@@ -10,7 +10,7 @@ sizes = [10, 50, 100, 250, 500]
 num_iter = 100
 
 # Results dictionary
-results = {'brute': {}}
+results = {'brute': {}, 'kadane': {}}
 
 # Iterate over sizes
 for size in sizes:
@@ -23,12 +23,18 @@ for size in sizes:
         max_subarray_brute_force(A)
     end_time = timeit.default_timer()
     results['brute'][size] = end_time - start_time
-
+    
+    # Time Kadane's algorithm
+    start_time = timeit.default_timer()
+    for _ in range(num_iter):
+        max_subarray_kadane(A)
+    end_time = timeit.default_timer()
+    results['kadane'][size] = end_time - start_time
 
     
 # Print results
 print("No. of iterations for timing:",num_iter)
 print("Results (time in seconds):")
-print("Size\tBrute force")
+print("Size\tBrute force\tKadane")
 for size in sizes:
-    print("{}\t{:.6f}".format(size, results['brute'][size]))
+    print("{}\t{:.6f}\t{:.6f}".format(size, results['brute'][size], results['kadane'][size]))
